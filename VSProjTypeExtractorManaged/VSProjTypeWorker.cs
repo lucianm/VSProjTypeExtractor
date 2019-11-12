83a3bb598a483592c147f2de2e4ed6d5419eb0d6
Lucian Muresan <lucianm@users.sourceforge.net>
2019-11-12 20:56:37 +0100

    VSProjTypeExtractorManaged: first version of C# assembly for determining the project type GUID of an existing project by using Visual Studio DTE automation for loading it to a temporary solution

---
+++ VSProjTypeExtractorManaged/VSProjTypeWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EnvDTE;
+
+namespace VSProjTypeExtractorManaged
+{
+    public class VSProjTypeWorker
+    {
+        public static string ExtractProjectTypeGuid(string projPath, int VS_MajorVersion)
+        {
+            string projTypeGuid = "";
+            try
+            {
+                // load project by silently automating the Visual Studio installation specified by the major version
+                string VisualStudioDTEVerString = String.Format("VisualStudio.DTE.{0}.0", VS_MajorVersion); 
+                Type visualStudioType = Type.GetTypeFromProgID(VisualStudioDTEVerString);
+                DTE dte = Activator.CreateInstance(visualStudioType) as DTE;
+                dte.MainWindow.Visible = false;
+                dte.SuppressUI = true;
+                dte.UserControl = false;
+                dte.Solution.Create(Environment.GetEnvironmentVariable("TEMP"), "Dummy.sln");
+                Project projLoaded = dte.Solution.AddFromFile(projPath);
+                projTypeGuid = projLoaded.Kind.ToString();
+                dte.Solution.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n{0}\n", ex.ToString());
+            }
+            return projTypeGuid;
+        }
+    }
+}
