0a32afb1a60add5d480c1d2ef2b0862c21710aae
Lucian Muresan <lucianm@users.sourceforge.net>
2019-11-12 22:39:37 +0100

    updated version information of DLLs

--- VSProjTypeExtractorManaged/Properties/AssemblyInfo.cs
+++ VSProjTypeExtractorManaged/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("VS automation for extracting project type GUID from existing project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSProjLoader")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("VSProjTypeExtractor")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Lucian Muresan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
