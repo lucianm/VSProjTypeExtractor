53f873894d14c164d859375235da9f8212a50820
Lucian Muresan <lucianm@users.sourceforge.net>
2019-11-13 13:12:52 +0100

    VSProjTypeExtractorManaged/VSProjTypeWorker.cs is now a singleton; VSProjTypeExtractor/VSProjTypeExtractor.cpp: thread safety when calling the singleton's ExtractProjectTypeGuid method by using a scoped lock

--- VSProjTypeExtractor/VSProjTypeExtractor.cpp
+++ VSProjTypeExtractor/VSProjTypeExtractor.cpp
@@ -1,6 +1,7 @@
 #include "VSProjTypeExtractor.h"
 
 #include <msclr/marshal.h>
+#include <msclr/lock.h>
 #include <string.h>
 
 
@@ -8,22 +9,29 @@
 namespace VSProjTypeExtractor {
 	public ref class ClassWorker
 	{
+	private:
+		ClassWorker(){}
+		ClassWorker(const ClassWorker%) { throw gcnew System::InvalidOperationException("ClassWorker cannot be copy-constructed"); }
+		static ClassWorker m_instance;
+		VSProjTypeExtractorManaged::VSProjTypeWorker m_managedWorker;
 	public:
+		static property ClassWorker^ Instance { ClassWorker^ get() { return % m_instance; } }
 		System::String^ GetProjTypeGuidStringManaged(System::String^ projPath, unsigned int vs_MajVer )
 		{
-			return VSProjTypeExtractorManaged::VSProjTypeWorker::ExtractProjectTypeGuid(projPath, vs_MajVer);
+			msclr::lock lock(this);
+			return m_managedWorker.ExtractProjectTypeGuid(projPath, vs_MajVer);
 		}
 	};
 }
 
+
 bool GetProjTypeGuidString(const char* projPath, char* projTypeGuid, unsigned int projTypeGuidMaxLength, unsigned int VS_MajorVersion)
 {
 	bool bSuccess = false;
-	VSProjTypeExtractor::ClassWorker work;
 	System::String^ strProjPath = gcnew System::String(projPath);
-	System::String^ strProjTypeGuid = work.GetProjTypeGuidStringManaged(strProjPath, VS_MajorVersion);
+	System::String^ strProjTypeGuid = VSProjTypeExtractor::ClassWorker::Instance->GetProjTypeGuidStringManaged(strProjPath, VS_MajorVersion);
 
-	if (strProjTypeGuid->Length >= VSPROJ_TYPEEXTRACT_MAXGUID_LENGTH - 1 && strProjTypeGuid->Length <= projTypeGuidMaxLength)
+	if (strProjTypeGuid->Length >= VSPROJ_TYPEEXTRACT_MAXGUID_LENGTH - 1 && unsigned int (strProjTypeGuid->Length) <= projTypeGuidMaxLength)
 	{
 		msclr::interop::marshal_context^ context = gcnew msclr::interop::marshal_context();
 		const char* str = context->marshal_as<const char*>(strProjTypeGuid);
--- VSProjTypeExtractorManaged/VSProjTypeWorker.cs
+++ VSProjTypeExtractorManaged/VSProjTypeWorker.cs
@@ -5,22 +5,42 @@ namespace VSProjTypeExtractorManaged
 {
     public class VSProjTypeWorker
     {
-        public static string ExtractProjectTypeGuid(string projPath, int VS_MajorVersion)
+        DTE dte;
+        bool dteInstanciated = false;
+
+        public VSProjTypeWorker()
+        {
+            dteInstanciated = false;
+        }
+
+        ~VSProjTypeWorker()
+        {
+            if (dteInstanciated)
+            {
+                dte.Solution.Close();
+                dteInstanciated = false;
+            }
+        }
+
+        public string ExtractProjectTypeGuid(string projPath, int VS_MajorVersion)
         {
             string projTypeGuid = "";
             try
             {
                 // load project by silently automating the Visual Studio installation specified by the major version
-                string VisualStudioDTEVerString = String.Format("VisualStudio.DTE.{0}.0", VS_MajorVersion); 
-                Type visualStudioType = Type.GetTypeFromProgID(VisualStudioDTEVerString);
-                DTE dte = Activator.CreateInstance(visualStudioType) as DTE;
-                dte.MainWindow.Visible = false;
-                dte.SuppressUI = true;
-                dte.UserControl = false;
-                dte.Solution.Create(Environment.GetEnvironmentVariable("TEMP"), "Dummy.sln");
+                if (!dteInstanciated)
+                {
+                    string VisualStudioDTEVerString = String.Format("VisualStudio.DTE.{0}.0", VS_MajorVersion);
+                    Type visualStudioType = Type.GetTypeFromProgID(VisualStudioDTEVerString);
+                    dte = Activator.CreateInstance(visualStudioType) as DTE;
+                    dte.MainWindow.Visible = false;
+                    dte.SuppressUI = true;
+                    dte.UserControl = false;
+                    dte.Solution.Create(Environment.GetEnvironmentVariable("TEMP"), "Dummy.sln");
+                    dteInstanciated = true;
+                }
                 Project projLoaded = dte.Solution.AddFromFile(projPath);
                 projTypeGuid = projLoaded.Kind.ToString();
-                dte.Solution.Close();
             }
             catch (Exception ex)
             {
